af0cbba303a2ab21aef19b4bec806de7411e453a
weepy <jonahfox@gmail.com>
2012-04-02 21:28:55 +0100

    more flesh on bones

--- o_O.memory_store.js
+++ o_O.memory_store.js
@@ -1,16 +1,13 @@
-// how do deal with loading id's and so should update the _id so that the unique id
-
-o_O.memory_store = function() {
+o_O.memoryStore = function() {
   this._id = 0
   this.objects = {}
   this.types = {}
 }
 
-var proto = o_O.memory_store.prototype
+var proto = o_O.memoryStore.prototype
 
-proto.uniqueId = function(namespace) {
-  var ret = ++this._id
-  return namespace ? namespace + ret : ret
+proto.uniqueId = function() {
+  return ++this._id
 }
 
 proto.find = function(id) {
@@ -19,19 +16,26 @@ proto.find = function(id) {
   
 proto.add = function(object) {
   var id = object.id
-  if(id == null || !o_O.memory_store.find(id) ) {
-    object.id = id || o_O.memory_store.unique_id()
+  if(id == null || !this.find(id) ) {
+    object.id = id || this.uniqueId()
     this.objects[id] = object
   }
 }
 
-// merge in
-o_O.model.protoype.store = new o_O.memory_store()
+proto.remove = function(o) {
+  delete this.objects[o.id]
+}
+
+// merge in some methods to o_O.model
+o_O.model.protoype.store = new o_O.memoryStore()
 o_O.model.protoype.save = function() {
   this.store.add(this)
 }
 
-// needs to be a factory also
-// o_O.store.create({id:10, type: 'Person', name: 'John' })
-// => creates a Person() with name() 'John'
-// this means we need to be able to register classes with the store
+// o_O.model already acts as a factory
+// doesn't makes sense to move this to o_O.store
+o_O.model.types = {...}
+o_O.model.create = function(){ ... } // perhaps better to call something like `instantiate` - although a bit long
+
+// how do deal with loading id's and so should update the _id so that the unique id
+// whenever adding an object - check to see if object.id >= this._id ?
